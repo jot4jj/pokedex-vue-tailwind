3b0c07a95697ded1d0d97c273c4231af3bdd5ff2
Jota <jsilvafreitas21@gmail.com>
2025-10-02 11:20:58 -0300

    feat: Adicionar sistema de busca e filtro de geração e tipo

--- src/components/molecules/CardPokemon.vue
+++ src/components/molecules/CardPokemon.vue
@@ -9,27 +9,19 @@
     <div v-else class="flex flex-col min-h-screen flex-grow">
       <div class="flex place-items-center justify-evenly">
         <div>
+
           <input
             type="text"
             v-model="searchTerm"
             placeholder="Buscar Pokémon..."
-            @keyup="buscarPokemonNome(searchTerm)"
             class="m-4 p-2 rounded-lg w-2/3 xl:w-full"
           />
         </div>
 
         <div>
-          <select
-            name="filtroTipo"
-            class="p-2"
-            v-model="selectedType"
-            @change="filtrarPorTipo(selectedType)"
-          >
-            <option value="">Todos os tipos</option>
-            <option 
-              v-for="type in tipos" 
-              :value="type"
-            >
+          <select name="filtroTipo" class="p-2" v-model="selectedType">
+            <option value="">Selecione o tipo</option>
+            <option v-for="type in tipos" :value="type">
               {{ type.charAt(0).toUpperCase() + type.slice(1) }}
             </option>
           </select>
@@ -37,78 +29,49 @@
           <select
             name="filtroGeracao"
             class="p-2"
-            v-model="selectedGeneration"
-            @change="filtrarPorGeracao(selectedGeneration)"
+            v-model.number="selectedGeneration"
           >
-            <option value="">Todas as gerações</option>
-            <option 
-              v-for="geracao in geracoes" 
-              :value="geracao"
-            > 
-              Geração{{ geracao }}
+            <option value="">Selecione a geração</option>
+            <option v-for="geracao in geracoes" :value="geracao">
+              Geração {{ geracao }}
             </option>
           </select>
-
-        </div>
+        </div> 
       </div>
 
       <div
-        class="grid grid-cols-1 place-items-center gap-12 mt-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:gap-24 xl:hover:transition"
+        class="grid grid-cols-1 place-items-center gap-12 mt-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:gap-24"
       >
-        <div v-if="searchResult" class="flex flex-col w-2/3">
-          <img
-            :src="searchResult.sprites.other['official-artwork'].front_default"
-            :alt="searchResult.name"
-          />
-          <div>
-            <p class="text-gray-600 text-xl xl:text-lg">
-              Nº{{ String(searchResult.id).padStart(4, "0") }}
-            </p>
-            <p class="text-gray-950 text-3xl pb-2 xl:text-2xl">
-              {{
-                searchResult.name.charAt(0).toUpperCase() +
-                searchResult.name.slice(1)
-              }}
-            </p>
-          </div>
+        <div class="border-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
-          <div class="grid grid-cols-2 gap-2">
+          <div
+            v-for="poke in buscarPokemon"
+            :key="poke.id"
+            class="flex gap-10"
+          >
             <img
-              v-for="(icon, idx) in searchResult.typeIcons"
-              :src="icon"
-              :key="idx"
-              class="rounded-lg"
+              :src="poke.sprites.other['official-artwork'].front_default"
+              :alt="poke.name"
             />
-          </div>
-        </div>
 
-        <div
-          v-for="poke in buscarPokemon"
-          :key="poke.id"
-          class="flex flex-col w-2/3"
-        >
-          <img
-            :src="poke.sprites.other['official-artwork'].front_default"
-            :alt="poke.name"
-          />
+            <div class="text-left p-4 tracking-wide">
+              <p class="text-gray-600 text-xl xl:text-lg">
+                Nº{{ String(poke.id).padStart(4, "0") }}
+              </p>
 
-          <div class="text-left p-4 tracking-wide">
-            <p class="text-gray-600 text-xl xl:text-lg">
-              Nº{{ String(poke.id).padStart(4, "0") }}
-            </p>
+              <p class="text-gray-950 text-3xl pb-2 xl:text-2xl">
+                {{ capitalize(poke.name) }}
+              </p>
 
-            <p class="text-gray-950 text-3xl pb-2 xl:text-2xl">
-              {{ poke.name.charAt(0).toUpperCase() + poke.name.slice(1) }}
-            </p>
-
-            <div class="grid grid-cols-2 gap-2">
-              <img
-                v-for="(icon, idx) in poke.typeIcons"
-                :key="idx"
-                :src="icon"
-                :alt="poke.typeName"
-                class="rounded-lg"
-              />
+              <div class="grid grid-cols-2 gap-2">
+                <img
+                  v-for="(icon, idx) in poke.typeIcons"
+                  :key="idx"
+                  :src="icon"
+                  :alt="poke.typeName"
+                  class="rounded-lg"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -119,79 +82,16 @@
 
 <script setup>
 import axios from "axios";
-import { ref, onMounted, computed, watch } from "vue";
+import { ref, onMounted, computed, capitalize, watch } from "vue";
 
-const pokemon = ref([]);
+const todosPokemon = ref([]);
 const searchTerm = ref("");
 const searchResult = ref(null);
+const selectedGeneration = ref("");
+const selectedType = ref("");
 const erro = ref(null);
 const loading = ref(true);
 
-async function fetchPokemon() {
-  try {
-    const listRes = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=12`);
-    const results = listRes.data.results;
-    const promises = results.map((p) => axios.get(p.url));
-    const details = await Promise.all(promises);
-
-    pokemon.value = details.map((d) => d.data);
-
-    for (const p of pokemon.value) {
-      p.typeName = p.types.map((t) => t.type.name);
-
-      const typeIconsPromises = p.types.map(async (t) => {
-        const typeRes = await axios.get(t.type.url);
-        return typeRes.data.sprites["generation-viii"]["sword-shield"].name_icon;
-      });
-
-      p.typeIcons = await Promise.all(typeIconsPromises);
-    }
-  } catch (erro) {
-    erro.value = "Erro ao buscar Pokemon.";
-    console.error(erro);
-  } finally {
-    loading.value = false
-  }
-}
-
-const buscarPokemon = computed(() => {
-  if (!searchTerm.value) return pokemon.value;
-  return pokemon.value.filter((p) =>
-    p.name.toLowerCase().includes(searchTerm.value.toLowerCase())
-  );
-});
-
-async function buscarPokemonNome(nome) {
-  try {
-    const res = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${nome.toLowerCase()}`
-    );
-    const p = res.data;
-
-    const typeIconsPromises = p.types.map(async (t) => {
-      const typeRes = await axios.get(t.type.url);
-      return typeRes.data.sprites["generation-viii"]["sword-shield"].name_icon;
-    });
-
-    p.typeIcons = await Promise.all(typeIconsPromises);
-
-    searchResult.value = p;
-    erro.value = null;
-
-  } catch (err) {
-    searchResult.value = null;
-    console.error(err);
-  }
-}
-
-watch(searchTerm, (novoValor) => {
-  if (!novoValor) {
-    searchResult.value = null;
-    erro.value = null;
-  }
-});
-
-const selectedType = ref("");
 const tipos = [
   "normal",
   "fire",
@@ -213,66 +113,110 @@ const tipos = [
   "fairy",
 ];
 
-async function filtrarPorTipo(tipo) {
-  if (!tipo) return fetchPokemon();
+const geracoes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+async function fetchPokemon(gen) {
   try {
-    const res = await axios.get(`https://pokeapi.co/api/v2/type/${tipo}`);
-    const lista = res.data.pokemon;
+    const listRes = await axios.get(`https://pokeapi.co/api/v2/generation/${gen}`);
+    const species = listRes.data.pokemon_species
 
-    const promises = lista.map((p) => axios.get(p.pokemon.url));
-
-    const details = await Promise.all(promises);
-
-    pokemon.value = details.map((d) => d.data);
-
-    for (const p of pokemon.value) {
-      p.typeName = p.types.map((t) => t.type.name);
+    const promises = species.map( async (pokeS) => {
+      const id = pokeS.url.split('/').filter(Boolean).pop()
+      const pokeRes = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const p = pokeRes.data
+      p.typeName = p.types.map((t) => t.type.name)
 
       const typeIconsPromises = p.types.map(async (t) => {
         const typeRes = await axios.get(t.type.url);
         return typeRes.data.sprites["generation-viii"]["sword-shield"].name_icon;
       });
 
-      p.typeIcons = await Promise.all(typeIconsPromises);
+      p.typeIcons = await Promise.all(typeIconsPromises)
+      return p
+    })
+
+    todosPokemon.value = await Promise.all(promises)
+
+  } catch (erro) {
+    erro.value = "Erro ao buscar Pokemon.";
+    console.error(erro);
+  } finally {
+    loading.value = false;
+  }
+}
+
+const buscarPokemon = computed(() => {
+
+  if (searchResult.value) {
+    return [searchResult.value];
+  }
+
+  let lista = todosPokemon.value;
+
+  if (selectedGeneration.value) {
+    const ranges = {
+      1: [1, 151],
+      2: [152, 251],
+      3: [252, 386],
+      4: [387, 493],
+      5: [494, 649],
+      6: [650, 721],
+      7: [722, 809],
+      8: [810, 905],
+      9: [906, 1010],
+    };
+    const [min, max] = ranges[selectedGeneration.value];
+    lista = lista.filter((p) => p.id >= min && p.id <= max);
+
+    if (selectedType.value) {
+      lista = lista.filter((p) => p.typeName.includes(selectedType.value));
     }
 
+  }
+
+  return lista;
+});
+
+async function buscarPokemonNome(nome) {
+
+  try {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${String(nome).toLowerCase()}`);
+    const p = res.data;
+
+    const typeIconsPromises = p.types.map(async (t) => {
+      const typeRes = await axios.get(t.type.url);
+      return typeRes.data.sprites["generation-viii"]["sword-shield"].name_icon;
+    });
+
+    p.typeIcons = await Promise.all(typeIconsPromises);
+
+    searchResult.value = p;
+    erro.value = null;
+
   } catch (err) {
+    searchResult.value = null;
     console.error(err);
   }
 }
-const selectedGeneration = ref('')
-const geracoes = [1, 2, 3, 4, 5, 6, 7, 8 ,9]
 
-async function filtrarPorGeracao(geracao) {
-  try {
-    const res = await axios.get(`https://pokeapi.co/api/v2/generation/${geracao}`)
-
-    const listaDeSpecies = res.data.pokemon_species
-
-    const promises = listaDeSpecies.map( s =>  axios.get(`https://pokeapi.co/api/v2/pokemon/${s.name}`))
-
-    const details = await Promise.all(promises)
-
-    pokemon.value = details.map(d => d.data)
-
-    for (const p of pokemon.value) {
-      p.typeName = p.types.map((t) => t.type.name);
-
-      const typeIconsPromises = p.types.map(async (t) => {
-        const typeRes = await axios.get(t.type.url);
-        return typeRes.data.sprites["generation-viii"]["sword-shield"].name_icon;
-      });
-
-      p.typeIcons = await Promise.all(typeIconsPromises);
-    }
-
-  } catch (err) {
-    console.error(err)
+watch(selectedGeneration, async (nova) => {
+  if (!nova) {
+    await fetchPokemon(1)
+  } else {
+    await fetchPokemon(nova)
   }
-}
+})
 
-onMounted(fetchPokemon);
+watch(searchTerm, (novoValor) => {
+  if (!novoValor) {
+    searchResult.value = null;
+    erro.value = null;
+  } else {
+    buscarPokemonNome(novoValor)
+  }
+});
+
+onMounted(() => fetchPokemon(1));
 </script>
 
 <style scoped>
