e8b797cccd76ecbb77152d15609522a3f9a08665
Jota <jsilvafreitas21@gmail.com>
2025-10-03 11:07:03 -0300

    chore: adicionar CardPokemon no PokemonList e atualizar rotas

---
+++ src/components/organisms/PokemonList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <PokemonModal />
+        <CardPokemon />
+    </div>
+</template>
+
+<script setup>
+import CardPokemon from '../molecules/CardPokemon.vue';
+import PokemonModal from '../molecules/PokemonModal.vue';
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Pokedex from '../views/Pokedex.vue'
 import Legendaries from '@/views/Legendaries.vue'
+import PokemonList from '@/components/organisms/PokemonList.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/Home', component: Home },
-  { path: '/Pokedex', component: Pokedex},
+  { path: '/Pokedex', component: PokemonList},
   { path: '/Legendaries', component: Legendaries}
 ]
 
