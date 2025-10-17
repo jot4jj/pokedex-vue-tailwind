2e20bc18265852fd395308012bca6258b57bfa8a
Jota <jsilvafreitas21@gmail.com>
2025-10-17 10:23:13 -0300

    chore: remover componente

--- src/components/molecules/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="absolute mt-6 bottom-0 bg-transparent">
-        <p>Made with ❤️ by <a href="https://github.com/jot4jj" target="_blank" class="text-blue-600 no-underline">João Vitor</a></p>
-    </div>
-</template>
